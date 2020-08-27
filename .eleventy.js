c0d3a0a734b94b370806da6109f28dfd90728921
James Randolph <jwrand16@gmail.com>
2020-08-27 11:21:35 -0400

    Updated eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = (eleventyConfig) => {
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addPassthroughCopy("src/site/_includes/css");
+  eleventyConfig.addPassthroughCopy("src/site/images");
   eleventyConfig.setTemplateFormats("html");
 
   return {
