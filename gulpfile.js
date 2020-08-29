b0a89d0fa1a6ffcb55497b2391cefd7a1e479c17
James Randolph <jwrand16@gmail.com>
2020-08-28 22:32:43 -0400

    Added image optimization script and optimized images

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,16 @@
 
 var Fiber = require("fibers");
 var gulp = require("gulp");
+var imagemin = require("gulp-imagemin");
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 
 sass.compiler = require("sass");
+
+// Optimize all the images
+gulp.task("optimize-images", () => {
+  return gulp.src("./src/**/*.{png,jpg,jpeg,gif,svg}").pipe(imagemin()).pipe(gulp.dest("src"));
+});
 
 // Generate the css with sass and autoprefixer
 gulp.task("css", () => {
